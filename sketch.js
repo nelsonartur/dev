b4a925298045014f08990ea223e79d01469cb3c7
healthyego <39567694+healthyego@users.noreply.github.com>
2018-05-25 15:17:16 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,39 +1,106 @@
-// Daniel Shiffman
-// https://www.kadenze.com/courses/the-nature-of-code
-// http://natureofcode.com/
-// Session 2: Array of Particles, multiple forces
+var spectrum;
+var sound;
 
-var person;
+var x=0;
+var y=0;
+var z=0;
+var counter=0;
+var r=0;
+var g=0;
+var b=0;
 
-//var obstacle;
+var rest=false;
 
-function setup() {
-  createCanvas(640, 360);
-  person = new Person();
+
+
+
+
+
+function setup(){
+  cnv= createCanvas(windowWidth,windowHeight,WEBGL);
+
+  background(0);
+  cnv.drop(gotFile);
+  fft = new p5.FFT(0.8,128);
+
+
 }
 
-function keyPressed() {
-  if (key == ' ') {
-    var jump = createVector(0, -5);
-    person.applyForce(jump);
+function gotFile(file){
+   sound= new p5.SoundFile(file.data, Play);
+}
+
+
+
+function Play(){
+  sound.loop();
+}
+
+function draw(){
+
+  //rest background
+  if(rest){
+    background(0);
+  }
+
+  spectrum = fft.analyze();
+
+  counter+=0.01;
+  r=80+(sin(counter)+1)*60
+  g=(sin(counter)+1)*50
+  b=(cos(counter+1)+1)*128
+  raster();
+}
+
+function raster(){
+
+
+  for(var i=0;i<spectrum.length;i++){
+    if (spectrum[i]>200-i*150/spectrum.length){
+      stroke(r,g,b);
+    }else{
+      noStroke();
+    }
+
+
+
+    //mit WEBGL
+    rotateX(millis()/1000);
+    rotateY(millis()/1500);
+    rotateZ(millis()/2000);
+
+    //vertical
+    line((i+1)*-width/(spectrum.length+1),-height,(i+1)*-width/(spectrum.length+1),height)
+    line((i+1)*width/(spectrum.length+1),-height,(i+1)*width/(spectrum.length+1),height)
+
+    //horizontal
+    line(-width,(i+1)*-height/(spectrum.length+1),width,(i+1)*-height/(spectrum.length+1))
+    line(-width,(i+1)*height/(spectrum.length+1),width,(i+1)*height/(spectrum.length+1))
+
+
+    //ohne WebGL
+    line((i+1)*width/(spectrum.length+1),0,(i+1)*width/(spectrum.length+1),height);
+    line(0,(i+1)*height/(spectrum.length+1),width,(i+1)*height/(spectrum.length+1));
   }
 }
 
-function draw() {
-  background(51);
-  
-  translate(-person.pos.x + 50, 0);
-
-  var gravity = createVector(0, 0.1);
-  person.applyForce(gravity);
 
 
-  person.update();
-  person.edges();
-  person.display();
-  
-  
 
-  fill(255, 0, 100);
-  rect(400, height - 50, 50, 50);
+
+
+
+function keyPressed() {
+  if(UP_ARROW){
+    x+=1
+  }
+  if(DOWN_ARROW){
+    z+=1
+  }
+  if(RIGHT_ARROW){
+    y+=1
+  }
+  if(ENTER){
+    rest=!rest
+  }
 }
