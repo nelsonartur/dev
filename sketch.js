24a4e9f20285f97dab5ef3f6dfe7e0e2bfea4a13
healthyego <39567694+healthyego@users.noreply.github.com>
2018-06-19 15:09:47 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,32 +1,106 @@
-var img;
-var pic;
-var a = 0;
+var spectrum;
+var sound;
 
-function setup() {
-  img = loadImage("healthyego.png");
-  pic = loadImage("healthyego_w.png");
-  createCanvas(windowWidth, windowHeight, WEBGL);
-  angleMode(DEGREES);
+var x=0;
+var y=0;
+var z=0;
+var counter=0;
+var r=0;
+var g=0;
+var b=0;
+
+var rest=false;
+
+
+
+
+
+
+function setup(){
+  cnv= createCanvas(windowWidth,windowHeight,WEBGL);
+
+  background(0);
+  cnv.drop(gotFile);
+  fft = new p5.FFT(0.8,128);
+
+
 }
 
-function draw() {
-  background(0);
+function gotFile(file){
+   sound= new p5.SoundFile(file.data, Play);
+}
 
 
-  if(a<=360){
-   a=a+0.5
-  }else if (a>360) {
-    a=0;
+
+function Play(){
+  sound.loop();
+}
+
+function draw(){
+
+  //rest background
+  if(rest){
+    background(0);
   }
 
-  console.log(a)
+  spectrum = fft.analyze();
 
-rotateY(a+90)
-  if(a<180){
-    texture(pic)
-  } else if(a>180){
-    texture(img)
+  counter+=0.01;
+  r=80+(sin(counter)+1)*60
+  g=(sin(counter)+1)*50
+  b=(cos(counter+1)+1)*128
+  raster();
+}
+
+function raster(){
+
+
+  for(var i=0;i<spectrum.length;i++){
+    if (spectrum[i]>200-i*150/spectrum.length){
+      stroke(r,g,b);
+    }else{
+      noStroke();
+    }
+
+
+
+    //mit WEBGL
+    rotateX(millis()/1000);
+    rotateY(millis()/1500);
+    rotateZ(millis()/2000);
+
+    //vertical
+    line((i+1)*-width/(spectrum.length+1),-height,(i+1)*-width/(spectrum.length+1),height)
+    line((i+1)*width/(spectrum.length+1),-height,(i+1)*width/(spectrum.length+1),height)
+
+    //horizontal
+    line(-width,(i+1)*-height/(spectrum.length+1),width,(i+1)*-height/(spectrum.length+1))
+    line(-width,(i+1)*height/(spectrum.length+1),width,(i+1)*height/(spectrum.length+1))
+
+
+    //ohne WebGL
+    line((i+1)*width/(spectrum.length+1),0,(i+1)*width/(spectrum.length+1),height);
+    line(0,(i+1)*height/(spectrum.length+1),width,(i+1)*height/(spectrum.length+1));
   }
+}
 
-  plane(windowWidth/4,windowWidth/4)
+
+
+
+
+
+
+function keyPressed() {
+  if(UP_ARROW){
+    x+=1
+  }
+  if(DOWN_ARROW){
+    z+=1
+  }
+  if(RIGHT_ARROW){
+    y+=1
+  }
+  if(ENTER){
+    rest=!rest
+  }
 }
