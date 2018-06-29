c65615817fb3231fcabf04f493778bdaf23014fa
healthyego <39567694+healthyego@users.noreply.github.com>
2018-06-29 13:31:00 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function preload(){
   HE =loadImage('HE.jpg');
 
   for (var i = 0; i< 10; i++){
-    pic1= loadImage("pics/"+i+".jpg");
+    pic1= loadImage("Lisa/IMG_"9180+i+".JPG");
     pictures1.push(pic1);
   }
 
