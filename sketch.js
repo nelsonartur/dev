af99d814a19833692e267554e18e1fa8aae6ad13
healthyego <39567694+healthyego@users.noreply.github.com>
2018-06-30 11:47:23 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
-var Value=120;
-var Length=35;
+var Value  =120;
+var Length =5;
 var song;
+var playing = false;
+var show = true;
 
-var counter=true;
 
 var pictures1=[];
 var pictures2=[];
@@ -12,55 +13,58 @@ var spectrum;
 
 function preload(){
   song = loadSound('sosi.mp3');
-  HE =loadImage('HE.jpg');
+  HE = loadImage('HE.jpg');
 
   for (var i = 0; i< 10; i++){
-    pic1= loadImage("pics/"+i+".jpg");
+    pic1= loadImage("pics/"+i+".jpg");  //pic1= loadImage("Lisa/"+i+".JPG");
     pictures1.push(pic1);
   }
-
 }
 
 
 function setup(){
-  cnv= createCanvas(windowWidth,windowHeight);
-  song.loop();
+  cnv = createCanvas(windowWidth,windowHeight);
   fft = new p5.FFT(0.8,64);
 }
 
 
+function mousePressed(){
+  togglePlay();
+}
+
 function draw(){
+   background(0);
+
+  if(show){
+    HealthyEgo();
+  }
 
 
 
-  console.log(Length)
-  background(0);
+
   spectrum = fft.analyze();
 
   //pictures
   if(spectrum[Length]>Value){
     Pictures1();
-    counter=!counter;
-  }
-
-  if(counter){
-    HealthyEgo();
   }
 
 }
 
 
+
 function Pictures1(){
   push();
-  imageMode(CENTER)
-  image(pictures1[round(random(0,pictures1.length-1))],windowWidth/2,windowHeight/2);
+  imageMode(CENTER);
+
+  image(pictures1[round(random(0,pictures1.length-1))],windowWidth/2,windowHeight/2,(windowWidth-1500),3456*((windowWidth-1500)/5184));
   pop();
 }
 
 function HealthyEgo(){
   push();
   imageMode(CENTER);
-  image(HE,windowWidth/2,windowHeight/2,);
+  image(HE,windowWidth/2,windowHeight/2,(windowWidth-1500),1748*((windowWidth-1500)/2480));
   pop();
 }
 
@@ -69,8 +73,18 @@ function tool(){
   for (var i = 0; i< spectrum.length; i++){
       var x = map(i, 0, spectrum.length, 0, width);
       var y =  map(spectrum[i], 0, 255, height, 0);
-
     }
 
-
 }
+
+function togglePlay() {
+  if (playing) {
+    song.pause();
+
+  } else {
+    song.loop();
+  }
+  playing = !playing;
+  show=!show;
+}
+
