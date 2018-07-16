18e2df63a0099a69bf4154a90b126f6a7a05150b
healthyego <39567694+healthyego@users.noreply.github.com>
2018-07-16 13:48:05 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -59,7 +59,7 @@ function draw(){
 
   if(show){
     HealthyEgo();
-    setTimeout(img.show(),2000);
+    setTimeout(showInstagram(),2000);
   }else{
   img.hide()};
 
@@ -80,11 +80,11 @@ function draw(){
    Pictures2();
    Value2+=1;
   }
- 
- 
-
 }
 
+function showInstagram(){
+ img.show()
+}
 
 
 function Pictures1(){
