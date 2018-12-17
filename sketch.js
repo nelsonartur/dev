ba6a30d2a4b1e87e69345835d82b58919449838c
healthyego <39567694+healthyego@users.noreply.github.com>
2018-12-17 13:18:05 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,31 +1,19 @@
 function setup(){
  imageMode(CENTER);
-var cnv = createCanvas(windowWidth,windowHeight);
- 
+  
+ var cnv = createCanvas(windowWidth,windowHeight);
  var img= createImg('HE.png','clickable');  
 }
 
 
-
 function mousePressed(){
-window.open("https://healthyego.github.io/info/");
+ window.open("https://healthyego.github.io/info/");
 }
-
-
-
-
-
-
-
 
 function draw(){
-   background(0);
+ background(0);
  HealthEgo();
 }
-
-
-
-
 
 function HealthyEgo(){
   push();
