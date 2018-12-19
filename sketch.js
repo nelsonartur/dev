b9972459892a077f81b52bca69d4b896df88d46b
healthyego <39567694+healthyego@users.noreply.github.com>
2018-12-19 16:45:30 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,34 +10,12 @@ img=loadImage('HE.png');
 }
 
 function draw(){
- r;g;b =random(100,180);
+ r =random(100,180);
+ g =random(100,180);
+ b =random(100,180);
  
  background(r,g,b);
 }
 
 
 
-/*function LINK(){
- window.open("http://www.healthyego.studio/info");
-}
-
-
-function setup() {
- cnv= createCanvas(windowWidth,windowHeight);
- img = loadImage('HE.png');
-
-r=random(100,180);
-g=random(100,180);
-b=random(100,180);
-
-}
-
-function draw() {
- 
- imageMode(CENTER);
- background(r,g,b);
- image(img,windowWidth/2,windowHeight/2,width,width/1.42);
-
- cnv.mousePressed(LINK());
-}
-*/
