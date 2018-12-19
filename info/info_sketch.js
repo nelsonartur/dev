b506bab0a446d7046aa5dbe25bdddd703ef5d2a3
healthyego <39567694+healthyego@users.noreply.github.com>
2018-12-19 16:43:41 +0100

    Update info_sketch.js

--- info/info_sketch.js
+++ info/info_sketch.js
@@ -1,4 +1,14 @@
+function mousePressed(){
+ window.open("https://instagram.com/haelthyego/")
+}
+
 function setup(){
-createCanvas(windowWidth, windowHeight);
-background(100);
+ img = loadImage('info.png');
+ createCanvas(windowWidth, windowHeight);
+ background(80);
+}
+
+function draw(){ 
+ imageMode(CENTER);
+ image(img,windowWidth/2,windowHeight/2,height/1.7777,height); 
 }
