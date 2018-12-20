8afa8f50fc0456cce2706f8918ca117f90b81523
healthyego <39567694+healthyego@users.noreply.github.com>
2018-12-20 09:48:53 +0100

    Update info_sketch.js

--- info/info_sketch.js
+++ info/info_sketch.js
@@ -1,15 +1,18 @@
 var img;
 
-function setup(){
- img = loadImage('info/info.png');
+function preload(){
+img = loadImage('info/info.png');
+}
+
+function setup(){ 
  createCanvas(windowWidth, windowHeight);
  background(80);
+ img.mousePressed(LINK());
 }
 
 function draw(){ 
  imageMode(CENTER);
  image(img,windowWidth/2,windowHeight/2,height/1.7777,height); 
- img.mouseClicked(LINK());
 }
 
 function LINK(){
