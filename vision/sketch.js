45865415f2a99ba0fe2b105e6871c5de023f862b
healthyego <39567694+healthyego@users.noreply.github.com>
2019-01-29 18:46:59 +0100

    Java-code for vision

---
+++ vision/sketch.js
@@ -0,0 +1,35 @@
+var cnv;
+var img1; 
+var b;
+var c;
+
+function setup (){
+cnv= createCanvas(windowWidth, windowHeight);
+img1=loadImage('frontpage/front.png');
+
+ cnv.mousePressed(LINK);
+}
+
+function draw(){
+ b = random(0,1);
+ 
+ if(b>0.5){
+ c=0;
+ }else{
+ c=255
+ }
+ 
+ background(0);
+ push()
+ imageMode(CENTER);
+ image(img1,windowWidth/2,windowHeight/2,height/1.77,height);
+ pop()
+}
+
+
+function LINK(){
+window.open("http://www.healthyego.studio/konkret");
+}
+
+
+
