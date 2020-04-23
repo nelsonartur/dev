16ef5422f1c39b02fd344ae404be003abbd86843
Paul Schlenzig - I18 <paul.schlenzig@nordakademie.de>
2020-04-23 11:45:45 +0200

    add arbeiten sub directory

--- frontpage/sketch.js
+++ frontpage/sketch.js
@@ -12,7 +12,7 @@
 //gewünschte Namen und amnzahl in Array schreiben
 //(!) .html dadei mit gleichen name muss im Backend vorhanden sein, sonst funktioniert link nicht
 var subpages = [];
-subpages=["events","association","socialmedia"];
+subpages=["events","arbeiten","association","socialmedia"];
 
 //Verzeichnisdomain, unterhalb welcher die Seiten aufgehängt werden
 var domainLevel = '../'; //var domainLevel = 'http://healthyego.studio/';
