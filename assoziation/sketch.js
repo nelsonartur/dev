f70f8c40f528ca948b18e668c58c48e2b64de1ec
Paul Schlenzig - I18 <paul.schlenzig@nordakademie.de>
2020-07-02 14:55:38 +0200

    add gedrucktes

--- assoziation/sketch.js
+++ assoziation/sketch.js
@@ -1,87 +1,31 @@
-//Hier wird die curtain Animation ersetellt:
-//dafür werden zwei html div als schwarze vierecke erzeugt,
-//die sobald das HE logo geladen wurde 1500ms warten und auseinaderlaufen,
-//nach 1500 ms wird das HE logo gelöscht, um während des Auseinaderlaufens den Hintergrund sichtbar zu halten.
-//Sind die beiden Vierecke jeweils am Rand angekommen, werden die Div elemente gelöscht,
-//um den späteren Code nicht zu beeinflussen.
-
-//Variablen Declaration_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
-var leftRec;//Vaiabelen für Linken Curtain(=HTML DIV Rechteck)
-var rightRec;//Vaiabelen für Rechten Curtain(=HTML DIV Rechteck)
-var counter=0; //"Schrittweite" des Auseinaderlaufes, dient auch als schwellenwert bei Löschen der Elemente
-var safetyOffset=20;//"Schnittrand" um zu verhindern, dass Hintergrund sichtbar ist wenn nicht gewollt
-var start=false;//boolean zum start der Animation, damit Animation nicht started bevor HELogo geladen wurde
-var logo;//Variable in der Logo gespeichert wird, wird gelöscht sobald Curtains auseinaderlaufen
-
-
-//Functions_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
-
-function setup(){
-  //nur Verwenden falls nicht schon in anderem Code ausgerufen; setupCurtain wird in setup von index.html ausgerufen
-  setupCurtain();
+let cnv;
+let d;
+let g;
+function setup() {
+  cnv = createCanvas(100, 100);
+  cnv.mouseWheel(changeSize); // attach listener for
+  // activity on canvas only
+  d = 10;
+  g = 100;
 }
 
-function setupCurtain(){
-  //Erstellt Linkes und Rechtes Html Div Element,
-  //läd HElogo und ruft go() aus, sobald HElogo fertig geladen wurde
-  leftRec = createDiv();
-  rightRec= createDiv();
-  logo= createImg('../source/logos/HElogo.png','','',go());
+function draw() {
+  background(g);
+  ellipse(width / 2, height / 2, d, d);
 }
 
-function draw(){
-  //ruft jeden Frame curtain() auf
-  curtain();
+// this function fires with mousewheel movement
+// anywhere on screen
+function mouseWheel() {
+  g = g + 10;
 }
 
-function curtain(){
-  //started erst wenn HElogo geladen wurde,
-  //warted 1500ms nachdem logo geladen wurde bevor eigentliche Animation beginnt;
-  //löscht logo nach 1.5s warten, um Hintergrund sichtbar zu machen;
-  //zählt counter hoch bis curtains aus dem Bild raus
-  //ruft curtainPosition() auf und übergibt aktuellen counter wert, mit dem die Poistion der Divs verändert wird
-  if(start){
-    logoStyle();
-    if (millis()>1500){
-      logo.remove();
-      if(counter<=windowWidth/2){
-        counter+=10;
-      }
-    }
+// this function fires with mousewheel movement
+// over canvas only
+function changeSize(event) {
+  if (event.deltaY > 0) {
+    d = d + 10;
+  } else {
+    d = d - 10;
   }
-  curtainPosition(counter);
-};
-
-function curtainPosition(counter){
-  //positioniert und stylet rechten und linken Curtain;
-  //Löscht Elemnte nachdem sie aus dem Bild sind und setzt start auf false um den counter zu stoppen
-  //Anmerkungen: Offset wichtig als "schnittrand"
-
-  leftRec.style('height', windowHeight+safetyOffset+'px');
-  leftRec.style('width' , windowWidth/2+safetyOffset-counter+'px');
-  leftRec.style('background-color','#000000');
-  leftRec.position(-safetyOffset+1, 0);
-
-  rightRec.style('height', windowHeight+safetyOffset+'px');
-  rightRec.style('width' , windowWidth/2+safetyOffset+counter+'px');
-  rightRec.style('background-color','#000000');
-  rightRec.position(windowWidth/2+counter, 0);
-
-  if(counter>=windowWidth/2){
-    rightRec.remove();
-    leftRec.remove();
-    start=false;
-  }
-}
-
-function go(){
-  //wird aufgerufen wenn das logo fertig geladen ist, um Animation zu starten
-  start=true;
-}
-
-function logoStyle(){
-  //positioniert das logo HOFFENTLICH zentral
-  //(!) Fehlerpotenzial
-  logo.style('max-width','100%');
-  logo.position(0,windowHeight/3);
 }
