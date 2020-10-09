5d2eaf9e3b2466801c09727afc5f0587f70bf398
Paul Jacob Schlenzig <paul.schlnezig@nordakademie.de>
2020-10-09 10:42:10 +0300

    add language switch button to hcc

--- arbeiten/gemeinnuetziges/sketch.js
+++ arbeiten/gemeinnuetziges/sketch.js
@@ -3,10 +3,14 @@ var cnv;
 var divHeight;
 var height;
 var img;
-
+var language;
+var englishText;
+var germanText;
 
 function preload() {
   img = loadImage('../source/arbeiten/gemeinnuetziges/BLM.png');
+  englishText= loadStrings('../source/arbeiten/gemeinnuetziges/englishText.txt');
+  germanText= loadStrings('../source/arbeiten/gemeinnuetziges/germanText.txt');
 }
 
 function setup (){
@@ -25,12 +29,32 @@ function setup (){
   cnv.position(0,0);
   cnv.style('top','0');
   cnv.style('left','50%');
+
+
+  button= select('button');
+  button.mousePressed(changeLanguage);
 }
 
 function draw(){
   if(counter<520){
       var size=random(20,60)
-      image(img,random(0,windowWidth),random(0,height),size, size);
+      image(img,random(0,windowWidth),random(windowHeight*0.1,height),size, size);
       counter+=1;
   }
+}
+
+function changeLanguage(){
+  var text= select('#text');
+
+  if(language == "English"){
+    language="German";
+    text.html(englishText);
+  }else{
+    language= 'English';
+    text.html(germanText);
+  }
+
+
+
+  button.html(language);
 }
